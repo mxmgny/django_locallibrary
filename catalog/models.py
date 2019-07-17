7b9e4f8c71e4c95831c114959103c68fbd13f036
Maksym Hnoinskyi <mxmgny@vivaldi.net>
2019-07-17 20:20:24 +0300

    language model addded

--- catalog/models.py
+++ catalog/models.py
@@ -14,6 +14,15 @@ class Genre(models.Model):
         return self.name
 
 
+class Language(models.Model):
+    name = models.CharField(
+        max_length=200,
+        help_text="Enter book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
@@ -23,6 +32,7 @@ class Book(models.Model):
         max_length=13,
         help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
