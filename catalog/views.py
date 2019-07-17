7a6eed84f186ba62868f8afa5be8fec4edc89955
Maksym Hnoinskyi <mxmgny@vivaldi.net>
2019-07-17 23:02:52 +0300

    Add Title block & more counters

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Book, BookInstance, Author
+from .models import Book, BookInstance, Author, Genre
 
 # Create your views here.
 
@@ -12,11 +12,16 @@ def index(request):
 
     num_authors = Author.objects.count()
 
+    num_genre_novel = Genre.objects.filter(name__icontains='novel').count()
+    num_books_war = Book.objects.filter(title__icontains='war').count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
-        'num_authors': num_authors
+        'num_authors': num_authors,
+        'num_genre_novel': num_genre_novel,
+        'num_books_war': num_books_war,
     }
 
     return render(request, 'index.html', context=context)
